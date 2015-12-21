cb737a69359f4b8b9f71ee0afbec1c2a962a9a95
HongleiXie <xhonglei2007@gmail.comg>
2015-12-21 00:07:39 -0500

    test

--- server.R
+++ server.R
@@ -1,30 +1,30 @@
-library(shiny)
-library(plyr)
 
-library("openxlsx");
-dt <- read.xlsx("~/Desktop/temp.xlsx", 
-                sheet = 1, 
-                startRow = 1, 
-                colNames = TRUE
-                );
+positions <- c('AUG', 'SEP', 'OCT', 'NOV');
 
-dt <- data.frame(lapply(dt, function(v) {
-  if (is.character(v)) return(toupper(v))
-  else return(v)
-    }
-  )
-)
+library(ggplot2);
+library(xlsx);
+library(shiny);
+Logged <- FALSE;
+PASSWORD <- data.frame(Brukernavn = "test", Passord = "202cb962ac59075b964b07152d234b70");
 
-# Define a server for the Shiny app
-shinyServer(function(input, output) {
-   
-  # Fill in the spot we created for a plot
-  output$phonePlot <- renderPlot({
-    df2 <- count(dt, vars = input$name);
-    # Render a barplot
-    barplot(df2$freq, 
-            main=input$name,
-            ylab="ds",
-            xlab="das")
+dir.data <- "~/Desktop/shiny_app";
+data <- read.xlsx(file.path(dir.data, 'data.xlsx'), sheetName = "Sheet1");
+
+shinyServer(function(input, output, session) {
+  
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    
+    subset(data, data$joint == input$type & data$bus == input$bus);
+    
   })
+  
+  
+  output$plot1 <- renderPlot({
+    
+    ggplot(selectedData(), aes(x=month, y=count, fill=type)) +  xlab("Meeting Date") + ylab("Number of Calls") +
+      geom_bar(stat="identity", position=position_dodge())+scale_x_discrete(limits = positions);
+    
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,43 +1,11 @@
-library(shiny)
-library("openxlsx");
-dt <- read.xlsx("~/Desktop/temp.xlsx", 
-                sheet = 1, 
-                startRow = 1, 
-                colNames = TRUE
-                );
+shinyUI(pageWithSidebar(
+  headerPanel('Test Test Test'),
+  sidebarPanel(
+    selectInput('bus', 'Business Lines', names(table(data$bus))),
+    selectInput('type', 'Calls Type', names(table(data$joint)))
 
-dt <- data.frame(lapply(dt, function(v) {
-  if (is.character(v)) return(toupper(v))
-  else return(v)
-    }
+  ),
+  mainPanel(
+    plotOutput('plot1')
   )
-)
-
-# Define the overall UI
-shinyUI(
-  
-  # Use a fluid Bootstrap layout
-  fluidPage(    
-    
-    # Give the page a title
-    titlePanel("TEST TITLE"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-      # Define the sidebar with one input
-      sidebarPanel(
-        selectInput("name", "LEVEL:", 
-                    choices=c('NAME_VP', 'NAME_ML', 'NAME_GL', 'Employee__HR_Full_Name')),
-        hr(),
-        helpText("Data TEST TEST")
-      ),
-      
-      # Create a spot for the barplot
-      mainPanel(
-        plotOutput("phonePlot")  
-      )
-      
-    )
-  )
-)
+))
