9e369f230a73cfa8a628ff233e54ab64bc31b8ab
HongleiXie <xhonglei2007@gmail.com>
2015-12-21 11:56:25 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,30 +1,56 @@
+library(ggplot2);
+library(openxlsx);
+library(shiny);
+Logged <- FALSE;
+PASSWORD <- data.frame(Brukernavn = "test", Passord = "BLACKOUT PASSWORD");
 
 positions <- c('AUG', 'SEP', 'OCT', 'NOV');
 
-library(ggplot2);
-library(xlsx);
-library(shiny);
-Logged <- FALSE;
-PASSWORD <- data.frame(Brukernavn = "test", Passord = "202cb962ac59075b964b07152d234b70");
+dir.data <- "C:\\Users\\s4120339\\Desktop\\shiny";
 
-dir.data <- "~/Desktop/shiny_app";
-data <- read.xlsx(file.path(dir.data, 'data.xlsx'), sheetName = "Sheet1");
+data <- read.xlsx(xlsxFile = file.path(dir.data, 'data.xlsx'), 
+                  sheet = 1, 
+                  startRow = 1, 
+                  colNames = TRUE,
+                  detectDates = TRUE,
+                  skipEmptyRows = TRUE,
+                  check.names = TRUE
+                  );
 
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output) {
+  source("www/Login.R",  local = TRUE)
   
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    
-    subset(data, data$joint == input$type & data$bus == input$bus);
+  observe({
+    if (USER$Logged == TRUE) {
+      
+      output$bus <- renderUI({
+        
+        selectInput('bus', 'Business Lines', names(table(data$bus)))
+        
+      })
+      
+      output$type <- renderUI({
+        
+        selectInput('type', 'Calls Type', names(table(data$joint)))
+        
+      })
+      
+      # Combine the selected variables into a new data frame
+      selectedData <- reactive({
+        
+        subset(data, data$joint == input$type & data$bus == input$bus);
+        
+      })
+      
+      
+      output$plot1 <- renderPlot({
+        
+        ggplot(selectedData(), aes(x=month, y=count, fill=type)) +  xlab("Meeting Date") + ylab("Number of Calls") +
+          geom_bar(stat="identity", position=position_dodge())+scale_x_discrete(limits = positions);
+        
+      })
+      
+    }
     
   })
-  
-  
-  output$plot1 <- renderPlot({
-    
-    ggplot(selectedData(), aes(x=month, y=count, fill=type)) +  xlab("Meeting Date") + ylab("Number of Calls") +
-      geom_bar(stat="identity", position=position_dodge())+scale_x_discrete(limits = positions);
-    
-  })
-  
 })
